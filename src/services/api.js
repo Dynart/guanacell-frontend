5c220bfaaba74a7f7c3db370d24c1b5c78dfb1a9
Dynart <dynart.95@gmail.com>
2025-10-19 17:16:40 -0600

    mejoras

--- src/services/api.js
+++ src/services/api.js
@@ -18,22 +18,22 @@ api.interceptors.request.use(config => {
 });
 
 export const getProducts = async () => {
-  const response = await api.get('/');
+  const response = await api.get('/api/products');
   return response.data;
 };
 
 export const createProduct = async (product) => {
-  const response = await api.post('/', product);
+  const response = await api.post('/api/products', product);
   return response.data;
 };
 
 export const updateProduct = async (id, product) => {
-  const response = await api.put(`/${id}`, product);
+  const response = await api.put(`/api/products/${id}`, product);
   return response.data;
 };
 
 export const deleteProduct = async (id) => {
-  await api.delete(`/${id}`);
+  await api.delete(`/api/products/${id}`);
 };
 
 export const register = async (userData) => {
