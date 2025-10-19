14300b62a96edcbfefaf1c440ff2f6fa9c1303db
Dynart <dynart.95@gmail.com>
2025-10-19 13:54:27 -0600

    Update API_URL to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.0.16:5000/api/products';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/products';
 
 const api = axios.create({
   baseURL: API_URL,
